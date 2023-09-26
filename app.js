938d4cd76be0ecd9384e5223076d43a4645a4476
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-09-26 14:47:29 -0400

    uodate

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // https://open-meteo.com
 
-let API_KEY = "digitar a key aqui";
+let API_KEY = "";
 let city_name = 'aquidauana';
 
-let lat, lon, weather;
+let lat, lon, weather, weatherForecast;
 
 //obter a latitude e longitude da cidade
 async function getLatLon(){
@@ -12,6 +12,7 @@ async function getLatLon(){
         const data = await response.json()
         lat = data[0].lat;
         lon = data[0].lon;
+        city_name = data[0].name;
         return { lat: lat, lon: lon };
 
     } catch (error) {
@@ -35,11 +36,34 @@ async function getWeather(lat, lon){
     }
 }
 
+async function getWeatherForecast(lat, lon){
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${API_KEY}`);
+        const data = await response.json()
+        weatherForecast = data;
+        return {weatherForecast: weatherForecast};
+
+    } catch (error) {
+        console.error("Ocorreu um erro:", error);
+        console.error("Erro:", error.response);
+    }
+}
+
 //função principal que pega os valores das funções para escrever no html
 async function writeInfoInHtml(){
     const { lat, lon } = await getLatLon();
     const { weather } = await getWeather(lat, lon);
+    const { weatherForecast } = await getWeatherForecast(lat, lon);
     console.log(weather);
+
+    const temperature = document.getElementById('temperature-now');
+    const city = document.getElementById('city');
+    const minTemp = document.getElementById('min-temp');
+    const maxTemp = document.getElementById('max-temp');
+    temperature.innerHTML = weather.main.temp.toFixed(0);
+    minTemp.innerHTML = `${weather.main.temp_min.toFixed(0)}°`;
+    maxTemp.innerHTML = `${weather.main.temp_max.toFixed(0)}°`;
+    city.innerHTML = city_name;
 }
 
 writeInfoInHtml();
